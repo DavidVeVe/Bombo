91d0fa68d82ff829f4415b960a8582c961be2972
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2024-02-20 13:46:28 -0600

    Add storybook dependency, create bombo theme and add base config

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import BomboTheme from "./themes/bombo";
+
+addons.setConfig({
+  theme: BomboTheme
+});
---
+++ .storybook/themes/bombo.ts
@@ -0,0 +1,12 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Bombo",
+  brandUrl: "https://example.com",
+  brandImage:
+    "https://images.pexels.com/photos/9705819/pexels-photo-9705819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  brandTarget: "_self",
+  colorPrimary: "#262626",
+  colorSecondary: "#fc6736"
+});
